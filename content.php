1b2947bc2780b588e71efb7c873b9c4b108e5a43
Utsav Sharma <sharmautsav2003@gmail.com>
2023-03-27 23:37:14 +0530

    minors

--- content.php
+++ content.php
@@ -1,35 +1,34 @@
 <html>
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content=
-		"width=device-width, initial-scale=1.0">
-	<title>div-1</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content=
+            "width=device-width, initial-scale=1.0">
+        <title>div-1</title>    
 
-<body style="align-items: left;">
-    document.getElementById("content").innerHTML='<?php
-        $x = $_REQUEST['id'];
-        $connection = mysqli_connect("localhost","root","")
-        or die ("Couldn't connect to server");
+                
+    </head>
 
-        $db = mysqli_select_db($connection,"iInventory")
-        or die ("Couldn't select database");
+    <body style="align-items: left;" id="material">
+            <?php
+                $x = $_REQUEST['id'];
+                $connection = mysqli_connect("localhost","root","")
+                or die ("Couldn't connect to server");
 
+                $db = mysqli_select_db($connection,"iInventory")
+                or die ("Couldn't select database");
 
-        $query = "SELECT * FROM ".$x;
-        $result = mysqli_query($connection,$query)
-        or die("Query failed: ".mysqli_error($connection));
+                $query = "SELECT * FROM ".$x;
+                $result = mysqli_query($connection,$query)
+                or die("Query failed: ".mysqli_error($connection));
 
-        echo "<ul";
-        while ($row = mysqli_fetch_array($result)){
-            echo "<li>", $row['S_No'], "&emsp;", $row['Name'], "</li>";
-        }
+                echo ("<ul>");
+                while ($row = mysqli_fetch_array($result)){
+                    echo "<li>",$row['S_No'],"&emsp;",$row['Name'],"</li>";
+                }
+                echo "</ul>";
 
-        echo "</ul>";
-
-        mysqli_close($connection);
-    ?>'
-</body>
-
+                mysqli_close($connection);
+            ?>
+    </body>
 </html>
